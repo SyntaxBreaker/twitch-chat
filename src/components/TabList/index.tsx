ec9875b918b4a1daecefae6d886610eefccb777c
SyntaxBreaker <60369185+SyntaxBreaker@users.noreply.github.com>
2024-03-11 15:06:23 +0100

    Implement scrollable tabs

--- src/components/TabList/index.tsx
+++ src/components/TabList/index.tsx
@@ -1,5 +1,6 @@
 import Tab from '../Tab';
 import '../../styles/TabList.scss';
+import React from 'react';
 
 interface IProps {
   channels: IChannels[];
@@ -10,14 +11,18 @@ interface IChannels {
   name: string;
 }
 
-function TabList({ channels }: IProps) {
-  return (
-    <div className="tablist">
-      {channels.map((channel) => (
-        <Tab channel={channel.name} key={channel.id} />
-      ))}
-    </div>
-  );
-}
+const TabList = React.forwardRef<HTMLDivElement, IProps>(
+  function TabList(props, ref) {
+    const { channels } = props;
+
+    return (
+      <div className="tablist" ref={ref}>
+        {channels.map((channel) => (
+          <Tab channel={channel.name} key={channel.id} />
+        ))}
+      </div>
+    );
+  },
+);
 
 export default TabList;
--- src/components/Tabs/index.tsx
+++ src/components/Tabs/index.tsx
@@ -1,34 +1,163 @@
+import { useEffect, useRef } from 'react';
 import addIcon from '../../../assets/add.svg';
+import leftArrow from '../../../assets/leftArrow.svg';
+import rightArrow from '../../../assets/rightArrow.svg';
 import '../../styles/Tabs.scss';
 import TabList from '../TabList';
 
 const CHANNELS = [
   {
     id: '0',
-    name: 'Test',
+    name: 'Aurora',
   },
   {
     id: '1',
-    name: 'Test2',
+    name: 'Zephyr',
   },
   {
     id: '2',
-    name: 'Test3',
+    name: 'Cassiopeia',
   },
   {
     id: '3',
-    name: 'Test4',
+    name: 'Orion',
   },
   {
     id: '4',
-    name: 'Someone',
+    name: 'Luna',
   },
+  {
+    id: '5',
+    name: 'Nova',
+  },
+  {
+    id: '6',
+    name: 'Phoenix',
+  },
+  {
+    id: '7',
+    name: 'Galaxy',
+  },
+  {
+    id: '8',
+    name: 'Stardust',
+  },
+  {
+    id: '9',
+    name: 'Celestia',
+  },
+  {
+    id: '10',
+    name: 'Nebula',
+  },
+  {
+    id: '11',
+    name: 'Lyra',
+  },
+  {
+    id: '12',
+    name: 'Aether',
+  },
+  {
+    id: '13',
+    name: 'Solstice',
+  },
+  {
+    id: '14',
+    name: 'Aquila',
+  },
+  {
+    id: '15',
+    name: 'Vega',
+  },
+  {
+    id: '16',
+    name: 'Polaris',
+  },
+  {
+    id: '17',
+    name: 'Andromeda',
+  },
+  {
+    id: '18',
+    name: 'Sirius',
+  },
+  {
+    "id": "19",
+    "name": "Eclipse"
+  },
+  {
+    "id": "20",
+    "name": "Supernova"
+  },
+  {
+    "id": "21",
+    "name": "Aurora Borealis"
+  },
+  {
+    "id": "22",
+    "name": "Pegasus"
+  },
+  {
+    "id": "23",
+    "name": "Comet"
+  },
+  {
+    "id": "24",
+    "name": "Spectra"
+  },
+  {
+    "id": "25",
+    "name": "Andromeda"
+  },
+  {
+    "id": "26",
+    "name": "Nebula"
+  },
+  {
+    "id": "27",
+    "name": "Asteria"
+  },
+  {
+    "id": "28",
+    "name": "Stellar"
+  },
+  {
+    "id": "29",
+    "name": "Quasar"
+  },
+  {
+    "id": "30",
+    "name": "Cosmos"
+  }
 ];
 
 function Tabs() {
+  const myRef = useRef<HTMLDivElement>(null);
+
+  const scrollTabs = (direction: 'left' | 'right') => {
+    if (myRef.current) {
+      if (direction === 'left') {
+        myRef.current.scrollBy(-50, 0);
+      } else {
+        myRef.current.scrollBy(50, 0);
+      }
+    } else {
+      return;
+    }
+  };
+
   return (
     <nav className="tabs">
-      <TabList channels={CHANNELS} />
+      <div className="tabs__container">
+        <button className="tablist__button" onClick={() => scrollTabs('left')}>
+          <img src={leftArrow} alt="" />
+        </button>
+        <TabList channels={CHANNELS} ref={myRef} />
+        <button className="tablist__button" onClick={() => scrollTabs('right')}>
+          <img src={rightArrow} alt="" />
+        </button>
+      </div>
       <button className="tabs__button">
         <img src={addIcon} />
       </button>
