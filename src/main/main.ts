f219e209146a212acefa0a3e67a8450ea24f41aa
SyntaxBreaker <60369185+SyntaxBreaker@users.noreply.github.com>
2024-03-13 13:39:02 +0100

    Remove unnecessary code

--- src/main/main.ts
+++ src/main/main.ts
@@ -26,12 +26,6 @@ class AppUpdater {
 
 let mainWindow: BrowserWindow | null = null;
 
-ipcMain.on('ipc-example', async (event, arg) => {
-  const msgTemplate = (pingPong: string) => `IPC test: ${pingPong}`;
-  console.log(msgTemplate(arg));
-  event.reply('ipc-example', msgTemplate('pong'));
-});
-
 ipcMain.on('addChannel', (_, data) => {
   const channel = data.toLowerCase();
   if (!files.checkIfFileExists('channels.json')) {
--- src/main/preload.ts
+++ src/main/preload.ts
@@ -2,7 +2,7 @@
 /* eslint no-unused-vars: off */
 import { contextBridge, ipcRenderer, IpcRendererEvent } from 'electron';
 
-export type Channels = 'ipc-example' | 'addChannel' | 'readChannels';
+export type Channels = 'addChannel' | 'readChannels';
 
 const electronHandler = {
   ipcRenderer: {
