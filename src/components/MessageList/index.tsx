fe05283d57b1b7a9be0cd6139b2f31024feda692
SyntaxBreaker <60369185+SyntaxBreaker@users.noreply.github.com>
2024-03-22 15:20:55 +0100

    Add scroll to last message

--- src/components/MessageList/index.tsx
+++ src/components/MessageList/index.tsx
@@ -1,13 +1,21 @@
 import { MessageItem } from '../../types/globalTypes';
 import Message from '../Message';
 import '../../styles/MessageList.scss';
+import { useEffect, useRef } from 'react';
 
 function MessageList({ messages }: { messages: MessageItem[] }) {
+  const ref = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    ref.current?.scrollIntoView({ behavior: 'smooth' });
+  }, [messages]);
+
   return (
     <section className="messageList">
       {messages.map((message) => (
         <Message message={message} key={message.ID} />
       ))}
+      <div ref={ref}></div>
     </section>
   );
 }
