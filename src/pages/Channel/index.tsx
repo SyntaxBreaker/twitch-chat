932095d48c039c51e7543d611e882c43547792b8
SyntaxBreaker <60369185+SyntaxBreaker@users.noreply.github.com>
2024-04-02 13:13:01 +0200

    Add empty state handling to channel page

--- src/pages/Channel/index.tsx
+++ src/pages/Channel/index.tsx
@@ -2,6 +2,8 @@ import { useParams } from 'react-router-dom';
 import MessageList from '../../components/MessageList';
 import { useEffect, useState } from 'react';
 import { MessageItem } from '../../types/globalTypes';
+import chatImage from '../../../assets/beginChat.svg';
+import '../../styles/ChatBox.scss';
 
 function Channel() {
   const [messages, setMessages] = useState<null | MessageItem[]>(null);
@@ -16,8 +18,15 @@ function Channel() {
     window.electron.ipcRenderer.sendMessage('readMessagesFromChannel', channel);
   }, [channel]);
 
-  return (
-    messages && messages?.length > 0 && <MessageList messages={messages} />
+  return messages && messages?.length > 0 ? (
+    <MessageList messages={messages} />
+  ) : (
+    <div className="chat-box">
+      <img src={chatImage} alt="" className="chat-box__img" />
+      <h1 className="chat-box__heading">
+        There are no messages on {channel}&apos;s channel.
+      </h1>
+    </div>
   );
 }
 
