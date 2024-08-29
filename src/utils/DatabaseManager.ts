9c163f57d0a0c71e1991e354f32f43943268b9b7
SyntaxBreaker <60369185+SyntaxBreaker@users.noreply.github.com>
2024-08-29 15:13:14 +0200

    Improve database file creation

--- src/utils/DatabaseManager.ts
+++ src/utils/DatabaseManager.ts
@@ -1,6 +1,5 @@
 import sqlite3 from 'sqlite3';
 import path from 'path';
-import fs from 'fs';
 
 const filename = 'database.db';
 
@@ -8,22 +7,20 @@ export default class DatabaseManager {
   db;
 
   constructor() {
-    if (!fs.existsSync(`./${filename}`)) {
-      fs.writeFileSync(filename, '');
-    }
-
     this.db = new sqlite3.Database(
       path.resolve(__dirname, `../../${filename}`),
-      sqlite3.OPEN_READWRITE,
+      sqlite3.OPEN_READWRITE | sqlite3.OPEN_CREATE,
       (err) => {
         if (err) {
           console.log(err.message);
           return;
         }
+
         this.db.run(`CREATE TABLE IF NOT EXISTS channels (
         id INTEGER PRIMARY KEY,
         name TEXT NOT NULL
       )`);
+
         this.db.run(`CREATE TABLE IF NOT EXISTS messages (
         ID TEXT UNIQUE NOT NULL,
         channel TEXT NOT NULL,
@@ -34,7 +31,6 @@ export default class DatabaseManager {
         sub BOOLEAN NOT NULL,
         vip BOOLEAN NOT NULL
       )`);
-        console.log(`Connected to the database.`);
       },
     );
   }
